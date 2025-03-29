ce8f93e71cccb0b4927fe40e089dc9e31dfcfd0d
Atitaya <Atitaya.promlers@gmail.com>
2025-03-29 22:42:49 +0700

    fix navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { useState, useContext } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link,
+  Outlet,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import Place from "./pages/Place/Place";
 import Login from "./pages/Login";
@@ -21,28 +27,41 @@ function App() {
   return (
     <AuthProvider>
       <Router>
-        <NavBar />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/places" element={<Place />} />
-          <Route path="/places/:placeId" element={<PlaceDetail />} />
-          <Route path="/plans/generate" element={<PlanGenerate />} />
-          <Route path="/plans/:planId" element={<PlanDetail />} />
-          <Route path="/plans/saved" element={<UserPlan />} />
+          <Route>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUpPage />} />
+          </Route>
 
-          <Route path="/profile" element={<UserProfile />} />
-          <Route path="/blogs" element={<BlogSearch />} />
-          <Route path="/blogs/create" element={<BlogCreate />} />
-          <Route path="/blogs/saved" element={<UserBlog />} />
-          <Route path="/blogs/:id" element={<BlogDetail />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUpPage />} />
+          <Route element={<NavBarWrapper />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/places" element={<Place />} />
+            <Route path="/places/:placeId" element={<PlaceDetail />} />
+            <Route path="/plans/generate" element={<PlanGenerate />} />
+            <Route path="/plans/:planId" element={<PlanDetail />} />
+            <Route path="/plans/saved" element={<UserPlan />} />
 
-          {/* <Route path="/plans/:id" element={<Home />} /> */}
+            <Route path="/profile" element={<UserProfile />} />
+            <Route path="/blogs" element={<BlogSearch />} />
+            <Route path="/blogs/create" element={<BlogCreate />} />
+            <Route path="/blogs/saved" element={<UserBlog />} />
+            <Route path="/blogs/:id" element={<BlogDetail />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUpPage />} />
+          </Route>
         </Routes>
       </Router>
     </AuthProvider>
   );
 }
+
+const NavBarWrapper = () => {
+  return (
+    <>
+      <NavBar />
+      <Outlet /> {/* This renders the child routes */}
+    </>
+  );
+};
 
 export default App;
